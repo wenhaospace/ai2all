44c6f0e0b300b91fdc02e629f124c6a80bc8cf97
Wendell <1925005015@qq.com>
2025-05-21 15:15:25 +0800

    init project

---
+++ ai2all/src/main/java/com/wendell/ai2all/Ai2allApplication.java
@@ -0,0 +1,13 @@
+package com.wendell.ai2all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ai2allApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ai2allApplication.class, args);
+	}
+
+}

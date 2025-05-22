b95c0e4008c33bd11804a9480c20df4a3479868b
Wendell <1925005015@qq.com>
2025-05-22 11:28:58 +0800

    transfer text to vectors and save it to postgres database successfully

--- ai2all/src/main/java/com/wendell/ai2all/Ai2allApplication.java
+++ ai2all/src/main/java/com/wendell/ai2all/Ai2allApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ai2allApplication {
 
+	private static final org.slf4j.Logger logger =
+			org.slf4j.LoggerFactory.getLogger(Ai2allApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Ai2allApplication.class, args);
 	}
